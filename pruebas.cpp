51db88f4ef5540fff466f8e141ad6a5b272c9c70
SiroBrt <sirobroton@gmail.com>
2023-03-11 18:41:51 +0100

    creo que lo tengo

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <string>
 using namespace std;
 
@@ -39,32 +40,83 @@ class Date{
     }
 };
 
+class Juego{
+    private:
+    vector <pair<int,string>> players;
+    public:
+    void addPlayer(int id,string password){
+        pair <int,string> input;
+        input.first=id;
+        input.second=password;
+        players.push_back(input);
+    }
+    string getPassword(int id){
+        string output;
+        for(int i=0;i<players.size();i++){
+            if(players[i].first==id){
+                output=players[i].second;
+                break;
+            }
+        }
+        return output;
+        /*for(auto i:players){
+            cout <<i.first <<"->" <<i.second <<endl;
+        }
+        return "done";*/
+    }
+};
+
 class Persona{
     private:
     Date cumple;
     int ID;
+    list <pair <Juego,string>> registradoEn;
     public:
-    void setID(int input){
-        ID=input;
+    static int poblacion;
+    Persona(){
+        poblacion++;
+        ID=poblacion;
     }
     void setBirthday(int num1,int num2, int num3){
         cumple.setDay(num1);
         cumple.setMonth(num2);
         cumple.setYear(num3);
     }
-    static void printBirthday(Persona input){
-        int day=input.cumple.getDay();
-        int month=input.cumple.getMonth();
-        int year=input.cumple.getYear();
+    int getID(){
+        return ID;
+    }
+    void addGame(Juego &game,string password){
+        pair <Juego,string> input;
+        input.first=game;
+        input.second=password;
+        registradoEn.push_back(input);
+        game.addPlayer(ID,password);
+    }
+    void showPassword(Juego game){
+        string password;
+        cout <<ID <<" is ";
+        password=game.getPassword(ID);
+        cout <<password;
+    }    
+    void printBirthday(){
+        int day=cumple.getDay();
+        int month=cumple.getMonth();
+        int year=cumple.getYear();
         cout <<(day>=10? "":"0") <<day <<":";
         cout <<(month>=10? "":"0") <<month <<":";
         cout <<(year>=1000? "":year>=100? "0":year>=10?"00":"000") <<year;      //if else pero con mas "clase" :)
     }
 };
 
-int main(){
-    Persona yo;
-    yo.setBirthday(50,21,308);
-    Persona::printBirthday(yo);
+int Persona::poblacion=0;
 
+int main(){
+    Persona yo,tu;
+    Juego amongUs;
+    //pruebas fechas
+    /*cout <<"yo=" <<yo.getID() <<" tu=" <<tu.getID() <<endl;
+    yo.printBirthday();*/
+    yo.addGame(amongUs,"something");
+    tu.addGame(amongUs,"another thing");
+    tu.showPassword(amongUs);
 }
