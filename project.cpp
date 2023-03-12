ce5c23f3ab30941cf34112a078371af87321fa26
SiroBrt <sirobroton@gmail.com>
2023-03-12 20:24:50 +0100

    menu

--- project.cpp
+++ project.cpp
@@ -6,20 +6,39 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+//cortesía de pruebas.cpp
 class Date{
     private:
     int day,month,year;
     public:
-    Date(int num1,int num2, int num3){
-        day=num1;
-        month=num2;
-        year=num3;
-    }
     Date(){
         day=1;
         month=1;
         year=1;
+
+    }
+    Date(int num1,int num2,int num3){
+        day=num1;
+        month=num2;
+        year=num3;
+    }
+    void setDay(int num){
+        day=num;
+    }
+    void setMonth(int num){
+        month=num;
+    }
+    void setYear(int num){
+        year=num;
+    }
+    int getDay(){
+        return day;
+    }
+    int getMonth(){
+        return month;
+    }
+    int getYear(){
+        return year;
     }
 };
 
@@ -50,6 +69,26 @@ private:
     vector <Trip> travels;
 };
 
+int mainMenu(){
+    string output;
+    //usamos string para aceptar cualquier input
+    cout <<"---------- Choose an option ----------\n1. Add new passenger's trip to a train\n2. Remove a passenger's trip from a train\n3. Show trips of a passenger \n4. Show list of passengers in a specific train\n5. Show an alphabetically ordered list of passengers in all the trains\n6. Show passengers of cities\n7. End program \n";
+    cin >>output;
+    //pasar string a int sin stoi (solo queremos del 1 al 7) y si no se puede devuelve 0
+    return (output=="1"? 1:output=="2"? 2:output=="3"? 3:output=="4"? 4:output=="5"? 5:output=="6"? 6:output=="7"? 7:0);
+}
+
 int main(){
-    cout <<"hello world";
+    int selection;
+    while(selection!=7){
+        selection=mainMenu();
+        switch (selection){
+            case 0:
+            cout <<"Please, choose a valid option" <<endl;
+            break;
+            case 7:
+            cout <<"Goodbye";
+            break;
+        }
+    }
 }
